61a710bde0eea8735fe97e79940c3af33125698b
PabloCarrai <pablohernanjuan@hotmail.com>
2024-10-22 19:50:22 -0300

    Factorizando comando mv

--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -2,10 +2,13 @@
 
 #	Clonamos el repo con el cual vamos a trabajar
 G=$(which git)
+M=$(which mv)
 R=https://github.com/PabloCarrai/wp-c-wt.git
 #	Clonamos el repo que vamos a usar
 $G clone --quiet $R
 
 C=$(find $PWD -type d -name wp-c-w*)
 #	Renombramos la carpeta y la movemos al home
-mv $C $HOME/$1 
+$M $C $HOME/$1 
+
+

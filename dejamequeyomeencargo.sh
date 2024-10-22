d15643390d0c9a6777ab330c5e2608badb784803
PabloCarrai <pablohernanjuan@hotmail.com>
2024-10-22 19:36:10 -0300

    Agregando comentario

--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -3,9 +3,9 @@
 #	Clonamos el repo con el cual vamos a trabajar
 G=$(which git)
 R=https://github.com/PabloCarrai/wp-c-wt.git
-
+#	Clonamos el repo que vamos a usar
 $G clone --quiet $R
 
 C=$(find $PWD -type d -name wp-c-w*)
-
+#	Renombramos la carpeta y la movemos al home
 mv $C $HOME/$1 

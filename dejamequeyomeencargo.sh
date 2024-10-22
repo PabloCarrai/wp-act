1931a63145e74e613a3ba64767b1659ad4d165db
PabloCarrai <pablohernanjuan@hotmail.com>
2024-10-22 20:04:42 -0300

    Arreglando atajos

--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -3,12 +3,11 @@
 #	Clonamos el repo con el cual vamos a trabajar
 G=$(which git)
 M=$(which mv)
+F=$(which find)
 R=https://github.com/PabloCarrai/wp-c-wt.git
 #	Clonamos el repo que vamos a usar
 $G clone --quiet $R
 
-C=$(find $PWD -type d -name wp-c-w*)
+C=$($F $PWD -type d -name wp-c-w*)
 #	Renombramos la carpeta y la movemos al home
 $M $C $HOME/$1 
-
-

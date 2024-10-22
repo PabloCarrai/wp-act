dba7c5d4dd565839d156c49c4664e3fc23fade08
PabloCarrai <pablohernanjuan@hotmail.com>
2024-10-22 20:39:03 -0300

    Arreglando variables

--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -4,10 +4,11 @@
 G=$(which git)
 M=$(which mv)
 F=$(which find)
+H=$HOME/$1
 R=https://github.com/PabloCarrai/wp-c-wt.git
 #	Clonamos el repo que vamos a usar
 $G clone --quiet $R
-
+#	Busco la carpeta del proyecto y va a una variable
 C=$($F $PWD -type d -name wp-c-w*)
 #	Renombramos la carpeta y la movemos al home
-$M $C $HOME/$1 
+$M $C $H

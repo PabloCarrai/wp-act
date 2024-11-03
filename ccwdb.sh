5be8c86d7848338a686b67e15cb5ca4c62e5d276
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-03 17:25:22 -0300

    Agregando info al script

--- ccwdb.sh
+++ ccwdb.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+#	Genero una clave aleatoria que luego uso en el script
 TAM=$(( RANDOM % 25+15 ))
 CLAVE=$(openssl rand -base64 $TAM)
 ORIG="WORDPRESS_DB_PASSWORD=wordpress"
 REST="WORDPRESS_DB_PASSWORD=$CLAVE"
 ORIG1="MYSQL_PASSWORD=wordpress"
 REST1="MYSQL_PASSWORD=$CLAVE"
-
+#	La idea es modificar las variables anteriores en este archivo"
 FILE=$(find / -name .env-wp  2>/dev/null)
 
 sed -i "s|${ORIG}|${REST}|g" "$FILE"

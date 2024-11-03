7e27c8b05f2f11854430dcd5abf553b648874ad2
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-03 10:29:16 -0300

    Arreglando reemplazo de texto en .env

--- scndb.sh
+++ scndb.sh
@@ -5,7 +5,7 @@ REST="MYSQL_DATABASE=$1"
 ORIG1="WORDPRESS_DB_NAME=wordpress"
 REST1="WORDPRESS_DB_NAME=$1"
 
-FILE=$HOME/$1/.env-Example
+FILE=$(find / -name .env-Example 2>/dev/null)
 
 sed -i "s|${ORIG}|${REST}|g" "$FILE"
 sed -i "s|${ORIG1}|${REST1}|g" "$FILE"

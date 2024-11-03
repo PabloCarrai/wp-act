8633cf9fa0dbe09b5cff38295c042d2dddea140f
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-03 17:26:39 -0300

    Agregando info al script

--- ccrdb.sh
+++ ccrdb.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#	Genero una clave aleatoria
 TAM=$(( RANDOM % 25+15 ))
 CLAVE=$(openssl rand -base64 $TAM)
 ORIG="MYSQL_ROOT_PASSWORD=rootpress"
 REST="MYSQL_ROOT_PASSWORD=$CLAVE"
-
+#	Canbio las variables en un archivo usando la clave"
 FILE=$(find / -name .env-wp  2>/dev/null)
 
 sed -i "s|${ORIG}|${REST}|g" "$FILE" 

0d4403663c5f8bbf48f89f840b91beea0418956e
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-03 17:23:29 -0300

    Agregando info al script

--- cndb.sh
+++ cndb.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#	Aca cambiamos los valores de las variables en un .env"
 ORIG="MYSQL_DATABASE=wordpress"
 REST="MYSQL_DATABASE=$1"
 ORIG1="WORDPRESS_DB_NAME=wordpress"
 REST1="WORDPRESS_DB_NAME=$1"
-
+#	En este caso es en este archivo .env-wp"
 FILE=$(find / -name .env-wp  2>/dev/null)
 
 

2837025875dff2053d413d8b92d46c685e1e6812
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-03 12:33:44 -0300

    Agregando el cambio de la clave de wp

---
+++ ccwdb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TAM=$(( RANDOM % 25+15 ))
+CLAVE=$(openssl rand -base64 $TAM)
+ORIG="WORDPRESS_DB_PASSWORD=wordpress"
+REST="WORDPRESS_DB_PASSWORD=$CLAVE"
+ORIG1="MYSQL_PASSWORD=wordpress"
+REST1="MYSQL_PASSWORD=$CLAVE"
+
+FILE=$(FILE=$(find / -name .env-wp  2>/dev/null))
+
+sed -i "s|${ORIG}|${REST}|g" "$FILE"
+sed -i "s|${ORIG1}|${REST1}|g" "$FILE"
--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -4,4 +4,4 @@ bash $(find . -type f -name cr.sh 2>/dev/null)
 bash $(find . -type f -name rc.sh 2>/dev/null) $1
 bash $(find . -type f -name cndb.sh 2>/dev/null) $1
 bash $(find . -type f -name ccrdb.sh 2>/dev/null)
-
+bash $(find . -type f -name ccwdb.sh 2>/dev/null)

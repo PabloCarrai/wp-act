a84cac6e82f919762110fce8d26a07909a9916f5
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-03 11:21:23 -0300

    Rehaciendo script

--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -1,14 +1,1 @@
 #!/bin/bash
-
-#	Clonamos el repo con el cual vamos a trabajar
-G=$(which git)
-M=$(which mv)
-F=$(which find)
-H=$HOME/$1
-R=https://github.com/PabloCarrai/wp-c-wt.git
-#	Clonamos el repo que vamos a usar
-$G clone --quiet $R
-#	Busco la carpeta del proyecto y va a una variable
-C=$($F $PWD -type d -name wp-c-w*)
-#	Renombramos la carpeta y la movemos al home
-$M $C $H

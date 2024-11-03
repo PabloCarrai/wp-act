cfb1491177fc4ec77380c6fe75fc7b68b65b8db7
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-03 10:54:25 -0300

    Cambiando nombre del archivo .env

--- scndb.sh
+++ scndb.sh
@@ -5,7 +5,9 @@ REST="MYSQL_DATABASE=$1"
 ORIG1="WORDPRESS_DB_NAME=wordpress"
 REST1="WORDPRESS_DB_NAME=$1"
 
-FILE=$(find / -name .env-Example 2>/dev/null)
+FILE=$(find / -name .env-wp  2>/dev/null)
+
 
 sed -i "s|${ORIG}|${REST}|g" "$FILE"
 sed -i "s|${ORIG1}|${REST1}|g" "$FILE"
+

a3cb687ac8270c8717dc26c8a3bde77ebf15fa5d
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-06 19:06:20 -0300

    Ajustando variables repetidas

--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-bash $(find . -type f -name cr.sh 2>/dev/null) 
-bash $(find . -type f -name rc.sh 2>/dev/null) $1
-bash $(find . -type f -name cndb.sh 2>/dev/null) $1
-bash $(find . -type f -name ccrdb.sh 2>/dev/null)
-bash $(find . -type f -name ccwdb.sh 2>/dev/null)
-bash $(find . -type f -name cne.sh 2>/dev/null)
+B=$(which bash)
+F=$(which find)
+
+$B $($F . -type f -name cr.sh 2>/dev/null) 
+$B $($F . -type f -name rc.sh 2>/dev/null) $1
+$B $($F . -type f -name cndb.sh 2>/dev/null) $1
+$B $($F . -type f -name ccrdb.sh 2>/dev/null)
+$B $($F . -type f -name ccwdb.sh 2>/dev/null)
+$B $($F . -type f -name cne.sh 2>/dev/null)
 

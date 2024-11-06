0a9b944fa6db7ca32f21125f598e279c2933ba13
PabloCarrai <pablohernanjuan@hotmail.com>
2024-11-06 19:12:12 -0300

    Comentando script principal

--- dejamequeyomeencargo.sh
+++ dejamequeyomeencargo.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+#	Simplificamos dos variables que usaremos luego
 B=$(which bash)
 F=$(which find)
-
-$B $($F . -type f -name cr.sh 2>/dev/null) 
+#	Aca viene el orden de como corren los script
+#	Clonamos un repo que usaremos
+$B $($F . -type f -name cr.sh 2>/dev/null)
+# 	Aca renombramos la carpeta del proyecto en el home del usuario
 $B $($F . -type f -name rc.sh 2>/dev/null) $1
+#	Sobre los .env generamos las claves aleatorias
 $B $($F . -type f -name cndb.sh 2>/dev/null) $1
+#	Igual pero para otra variable
 $B $($F . -type f -name ccrdb.sh 2>/dev/null)
+#	Seguimos haciendo algo parecido
 $B $($F . -type f -name ccwdb.sh 2>/dev/null)
+#	Aca renombramos los .env
 $B $($F . -type f -name cne.sh 2>/dev/null)
 
